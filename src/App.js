18ccbf60d44f391291fe82022376e78a908755e1
Shaneel Kumar <shaneel_kumar@live.com>
2022-08-18 20:44:04 +1200

    Personal form refactored.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, Component } from "react";
 import uniqid from "uniqid";
 import { ThemeProvider } from "styled-components";
 import ReactToPrint from "react-to-print";
@@ -36,105 +36,113 @@ const theme = {
   },
 };
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      firstName: "",
-      lastName: "",
-      currentTitle: "",
-      age: "",
-      mobile: "",
-      email: "",
-      profilePhoto: null,
-      aboutMe: "",
-      jobList: [],
-      schoolList: [],
-      skillsList: [],
-      hobbiesList: [],
-      awardsList: [],
-      viewForms: true,
-      viewPDF: false,
-    };
+function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [email, setEmail] = useState("");
+  const [profilePhoto, setProfilePhoto] = useState(null);
+  const [aboutMe, setAboutMe] = useState("");
+  const [jobList, setJobList] = useState([]);
+  const [schoolList, setSchoolList] = useState([]);
+  const [skillsList, setSkillsList] = useState([]);
+  const [hobbiesList, setHobbiesList] = useState([]);
+  const [awardsList, setAwardsList] = useState([]);
+  const [viewForms, setViewForms] = useState(true);
+  const [viewPDF, setViewPDF] = useState(false);
+
+  // setMainState = (stateInfo) => {
+  //   const keys = Object.keys(stateInfo);
+  //   keys.forEach((key) => {
+  //     this.setState({
+  //       [key]: stateInfo[key],
+  //     });
+  //   });
+  // };
+
+  // appendToListState = (listID, item) => {
+  //   this.setState({
+  //     [listID]: [...this.state[listID], item],
+  //   });
+  // };
+
+  // setAboutMe = (description) => {
+  //   this.setState({
+  //     aboutMe: description,
+  //   });
+  // };
+
+  // deleteListItem = (listID, value) => {
+  //   this.setState({
+  //     [listID]: [...this.state[listID].filter((item) => item !== value)],
+  //   });
+  // };
+
+  // setProfilePhoto = (url) => {
+  //   this.setState({
+  //     profilePhoto: url,
+  //   });
+  // };
+
+  const toggleViews = () => {
+    setViewForms(viewForms ? false : true);
+    setViewPDF(viewPDF ? false : true);
+  };
+
+  const changeTextState = (e, cb) => {
+    cb(e.target.value)
   }
 
-  setMainState = (stateInfo) => {
-    const keys = Object.keys(stateInfo);
-    keys.forEach((key) => {
-      this.setState({
-        [key]: stateInfo[key],
-      });
-    });
-  };
+  // editText = (event) => {
+  //   const val = window.prompt("What would you like to edit this text to?");
+  //   const text = event.target;
+  //   text.innerText = val;
+  // };
 
-  appendToListState = (listID, item) => {
-    this.setState({
-      [listID]: [...this.state[listID], item],
-    });
-  };
+  // componentRef = null;
 
-  setAboutMe = (description) => {
-    this.setState({
-      aboutMe: description,
-    });
-  };
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
 
-  deleteListItem = (listID, value) => {
-    this.setState({
-      [listID]: [...this.state[listID].filter((item) => item !== value)],
-    });
-  };
-
-  setProfilePhoto = (url) => {
-    this.setState({
-      profilePhoto: url,
-    });
-  };
-
-  toggleViews = () => {
-    this.setState({
-      viewForms: this.state.viewForms === true ? false : true,
-      viewPDF: this.state.viewPDF === true ? false : true,
-    });
-  };
-
-  editText = (event) => {
-    const val = window.prompt("What would you like to edit this text to?");
-    const text = event.target;
-    text.innerText = val;
-  };
-
-  componentRef = null;
-
-  render() {
-    return (
-      <ThemeProvider theme={theme}>
-        <>
-          <GlobalStyles />
-
-          <FormSection viewStatus={this.state.viewForms}>
-            <div>
-              <PersonalForm
-                formSubmit={this.setMainState}
-                submittedInfo={this.state}
-              />
-              <AboutMe
+        <FormSection viewStatus={viewForms}>
+          <div>
+            <PersonalForm>
+              <div>
+                <label>First Name:</label>
+                <input onChange={e => changeTextState(e, setFirstName)}></input>
+              </div>
+              <div>
+                <label>Last Name:</label>
+                <input onChange={e => changeTextState(e, setLastName)}></input>
+              </div>
+              <div>
+                <label>Mobile:</label>
+                <input onChange={e => changeTextState(e, setMobile)}></input>
+              </div>
+              <div>
+                <label>Email:</label>
+                <input onChange={e => changeTextState(e, setEmail)}></input>
+              </div>
+            </PersonalForm>
+            {/* <AboutMe
                 formSubmit={this.setAboutMe}
                 submittedValue={this.state.aboutMe}
-              ></AboutMe>
-              <JobForm
+              ></AboutMe> */}
+            {/* <JobForm
                 formSubmit={this.appendToListState}
                 history={this.state.jobList}
                 deleteListItem={this.deleteListItem}
-              />
-              <SchoolForm
+              /> */}
+            {/* <SchoolForm
                 formSubmit={this.appendToListState}
                 history={this.state.schoolList}
                 deleteListItem={this.deleteListItem}
-              />
-            </div>
+              /> */}
+          </div>
 
-            <div>
+          {/* <div>
               <PhotoUpload
                 setPhoto={this.setProfilePhoto}
                 photo={this.state.profilePhoto}
@@ -158,32 +166,31 @@ class App extends Component {
                 history={this.state.awardsList}
                 deleteListItem={this.deleteListItem}
               />
-            </div>
+            </div> */}
 
-            <div>
-              <StyledLogo className="logo">CV</StyledLogo>
-              <NavButtons onClick={this.toggleViews}>Preview PDF</NavButtons>
-            </div>
-          </FormSection>
+          <div>
+            <StyledLogo className="logo">CV</StyledLogo>
+            <NavButtons onClick={toggleViews}>Preview PDF</NavButtons>
+          </div>
+        </FormSection>
 
-          <PDFSection viewStatus={this.state.viewPDF}>
-            <DummyPage
-              ref={(el) => (this.componentRef = el)}
-              details={this.state}
-            ></DummyPage>
-            <StyledLogo className="logo">PDF</StyledLogo>
-            <NavButtons onClick={this.toggleViews}>Edit CV</NavButtons>
-            <ReactToPrint
-              trigger={() => {
-                return <NavButtons>Print PDF</NavButtons>;
-              }}
-              content={() => this.componentRef}
-            />
-          </PDFSection>
-        </>
-      </ThemeProvider>
-    );
-  }
+        <PDFSection viewStatus={viewPDF}>
+          {/* <DummyPage
+            ref={(el) => (this.componentRef = el)}
+            details={this.state}
+          ></DummyPage>
+          <StyledLogo className="logo">PDF</StyledLogo> */}
+          <NavButtons onClick={toggleViews}>Edit CV</NavButtons>
+          {/* <ReactToPrint
+            trigger={() => {
+              return <NavButtons>Print PDF</NavButtons>;
+            }}
+            content={() => this.componentRef}
+          /> */}
+        </PDFSection>
+      </>
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- src/components/PersonalForm.js
+++ src/components/PersonalForm.js
@@ -1,75 +1,45 @@
-import { Form } from "./FormMethods";
-import { Input } from './Inputs'
-import { NavButtons } from './GeneralComponents'
+import styled from "styled-components";
 
-import { StyledMainForm } from './styles/StyledForms.styled'
-
-class PersonalForm extends Form {
-    state = {
-        firstName: '',
-        lastName: '',
-        currentTitle: '',
-        age: '',
-        mobile: '',
-        email: '',
-    }
-
-    render() {
-        const { firstName, lastName, currentTitle, age, mobile, email } = this.state
-        const submittedFirstName = this.props.submittedInfo.firstName
-        const submittedLastName = this.props.submittedInfo.lastName
-        const submittedCurrentTitle = this.props.submittedInfo.currentTitle
-        const submittedAge = this.props.submittedInfo.age
-        const submittedMobile = this.props.submittedInfo.mobile
-        const submittedEmail = this.props.submittedInfo.email
-        return (
-            <StyledMainForm onSubmit={this.submitState}>
-            <Input 
-                label='First Name:' 
-                field='firstName' 
-                updateFunction={this.updateState} 
-                defaultValue={firstName} 
-                submittedValue={submittedFirstName} 
-            />
-            <Input 
-                label='Last Name' 
-                field='lastName' 
-                updateFunction={this.updateState} 
-                defaultValue={lastName} 
-                submittedValue={submittedLastName} 
-            />
-            <Input 
-                label='Title:' 
-                field='currentTitle' 
-                updateFunction={this.updateState} 
-                defaultValue={currentTitle} 
-                submittedValue={submittedCurrentTitle} 
-            />
-            <Input 
-                label='Age:' 
-                field='age' 
-                updateFunction={this.updateState} 
-                defaultValue={age} 
-                submittedValue={submittedAge} 
-            />
-            <Input 
-                label='Mobile:' 
-                field='mobile' 
-                updateFunction={this.updateState} 
-                defaultValue={mobile} 
-                submittedValue={submittedMobile} 
-            />
-            <Input 
-                label='Email:' 
-                field='email' 
-                updateFunction={this.updateState} 
-                defaultValue={email} 
-                submittedValue={submittedEmail} 
-            />
-            <NavButtons>Add Personal Details</NavButtons>
-            </StyledMainForm>
-        )
-    }
+function PersonalForm(props) {
+  return (
+    <StyledMainForm>
+      {props.children}
+    </StyledMainForm>
+  );
 }
 
-export default PersonalForm
+const StyledMainForm = styled.form`
+  color: ${({ theme }) => theme.colors.mainBlue};
+  background: ${({ theme }) => theme.colors.formBG};
+  padding: 1rem;
+  border-radius: 0.5rem;
+  min-height: 150px;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  div {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  label {
+    color: ${({ theme }) => theme.colors.mainBlue};
+  }
+
+  input {
+    background: ${({ theme }) => theme.gradient.inputField};
+    flex: 1;
+    border: none;
+    border-top: 1px solid rgba(255, 255, 255, 1);
+    border-left: 1px solid rgba(255, 255, 255, 1);
+    border-radius: 0.5rem;
+    height: 2rem;
+    font-size: 1rem;
+    padding-left: 1rem;
+  }
+`;
+
+export default PersonalForm;
